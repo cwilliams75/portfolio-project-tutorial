9ea809a625df9b4698800d761f654ce7a495b463
CWilliams75 <coreywllms9@gmail.com>
2021-02-15 02:49:38 -0500

    Navbar completed

---
+++ src/components/Navbar.js
@@ -0,0 +1,61 @@
+import React from "react";
+import myLogo from "../Dad.jpg";
+// REACT FONT AWESOME IMPORTS
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {faBars} from "@fortawesome/free-solid-svg-icons";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-dark">
+      <div className="container">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">
+            <img className="myLogo" src={myLogo} alt="myLogo..." />
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation">
+           <FontAwesomeIcon icon={faBars} style={{ color: "#fff" }}/>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav ml-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#">Home</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">about</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">services</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">user stories</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">comments</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">contacts</a>
+              </li>
+
+            </ul>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
+
+{
+  /* <form className="d-flex">
+        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button className="btn btn-outline-success" type="submit">Search</button>
+      </form> */
+}
